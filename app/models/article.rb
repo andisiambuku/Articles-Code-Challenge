fcd4ac2765d14b38142c3b74997eb949b7d54be2
andisiambuku <roselandambuku@gmail.com>
2022-11-01 12:44:18 +0300

    feature:article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,24 @@
 class Article
 
+    attr_accessor :author, :magazine, :title
+    @@all = []
+
+    def initialize(author:,magazine:,title:)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def article_title
+        return
+    end
+
+    def all_articles
+        return
+    end
 
 end
