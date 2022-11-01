0f9ef3686ac5bc2600fe6d5650aae3af3dcb6320
andisiambuku <roselandambuku@gmail.com>
2022-11-01 16:47:29 +0300

    feature:author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,5 +8,26 @@ class Author
     @@all << self
   end
 
+  def all_articles
+    Article.all.filter {
+      |article| article.author == self
+    }
+  end
+  def add_new_article(magazine:,title:)
+    Article.new(self,magazine,title)
+  end
+  def all_magazine
+    self.articles.map {
+      |article| article.magazine
+    }.uniq
+  end
+  def magazine_category
+    self.magazine.map{
+      |magazine| magazine.category
+    }.uniq
+  end
 
+  def self_all
+    @@all
+  end
 end
